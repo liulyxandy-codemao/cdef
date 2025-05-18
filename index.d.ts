17e1436d2b1a14d621b78d3f75a3f5b86ae91da7
刘lyxAndy <liuyuxuanandy@outlook.com>
2025-05-18 12:55:59 +0800

    feat: 更新控件属性

--- index.d.ts
+++ index.d.ts
@@ -33,17 +33,98 @@ declare class VisibleWidget extends InvisibleWidget {
 }
 
 interface IDropdownOption {
+    /**
+     * 定义属性值下拉选项文本
+     */
     label: string;
+    /**
+     * 定义属性下拉选项值，属性内唯一，标识不同属性值选项
+     */
     value: string;
 }
 
+type ValidatorType<T> = T | {
+    value: Text,
+    message: string
+}
+
 interface IPropertyType {
+    /**
+     * 定义属性名称，控件内唯一，标识不同属性（遵循JavaScript命名规范）
+     */
     key: string;
+    /**
+     * 定义属性积木文本。
+     * 
+     * 参见：[可见控件内置属性](https://codemao.yuque.com/kzbwh0/coco_guide/idofvgf35gkisg9d#CPRor)
+     */
     label: string;
+    /**
+     * 定义属性类型，影响编辑器内积木嵌套和属性面板编辑
+     */
     valueType: string;
+    /**
+     * 定义属性数据类型，影响编辑器内积木嵌套
+     */
+    checkType?: string | string[];
+    /**
+     * 定义属性编辑器类型，影响属性面板编辑
+     */
     editorType?: string;
+    /**
+     * 功能	定义属性默认值
+     */
     defaultValue: any;
+    /**
+     * 定义属性编辑器类型，影响属性面板编辑
+     */
+    hidePropertyEditor?: boolean;
+    /**
+     * 定义属性是否只读
+     */
+    readonly?: boolean;
+    /**
+     * 定义属性 `valueType` 为 `number` 时的单位
+     */
+    unit?: string;
+    /**
+     * 定义属性值检查器
+     */
+    validators?: {
+        /**
+         * 检查属性 valueType 为 'number' 时是否小于指定值
+         */
+        lessThan?: ValidatorType<number>;
+        /**
+         * 检查属性 valueType 为 'number' 时是否大于指定值
+         */
+        greaterThan?: ValidatorType<number>;
+        /**
+         * 检查属性 valueType 为 'number' 时是否为整数
+         */
+        isInteger?: ValidatorType<boolean>;
+        /**
+         * 检查属性 valueType 为 'string' 时是否不为空
+         */
+        notEmpty?: ValidatorType<boolean>;
+        /**
+         * 检查属性 valueType 为 'string' 时是否大于指定长度
+         */
+        maxLength?: ValidatorType<number>;
+        /**
+         * 检查属性 valueType 为 'string' 时是否小于指定长
+         */
+        minLength?: ValidatorType<number>;
+        [x: string]: ValidatorType<any>;
+    }
+    /**
+     * 功能	定义属性值下拉列表
+     */
     dropdown?: IDropdownOption[];
+    /**
+     * 定义积木提示，鼠标悬浮在积木上显示
+     */
+    tooltip?: string;
     blockOptions?: {
         [x: string]: any;
     };
